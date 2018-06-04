1cecfaa4f96a64f0aaa41d0e0443a6e1ff7db144
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-06-04 17:00:34 -0400

    basemap with coordinates added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-var map = L.map('map').setView([40.773294,-73.907862], 14);
+console.log ('mymap');
+
+var mymap = L.map('map').setView([40.771117, -73.910651], 14);
+
+L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
