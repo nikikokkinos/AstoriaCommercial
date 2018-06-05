3665ffe7177468008ce33ac93ef37187e4c386fa
Niki Kokkinos <Niki@Nikis-MacBook-Air.local>
2018-06-05 15:49:27 -0400

    added data layers

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 console.log ('mymap');
 
-var mymap = L.map('map').setView([40.771117, -73.910651], 14);
+var mymap = L.map('map').setView([40.771117, -73.910651], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', {
+L.tileLayer('https://b.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png', {
 	maxZoom: 18,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+var CouncilDistrictOverlay = L.geoJSON (cd22,{
+    fillColor: "#ffe77c",
+    fillOpacity: .1,
+    weight: 1,
+  }).addTo(mymap);
+
+var NonConformingOverlay = L.geoJSON (nonconforming, {
+  fillColor: "#c13432",
+  fillOpacity: .5,
+  weight: 1,
 }).addTo(mymap);
